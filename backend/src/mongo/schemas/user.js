077f43ddb5a2e99cb76d8e5b52396fe3b4c59a6d
Fer Rosal <fernando.rosal@gmail.com>
2022-12-20 01:02:15 +0100

    fixed

--- backend/src/mongo/schemas/user.js
+++ backend/src/mongo/schemas/user.js
@@ -4,6 +4,17 @@ const userSchema = new Schema({
     name:{type : String},
     birthDate:{Type :String},
     email:{type: String},
+    role:{type: String},
+    status:{type:Number, min:1, max:5},
+    tratamiento: String,
+    genero:String,
+    fumador:Boolean,
+    hablador:{type:Number, min:1, max:3},
+    musica:{type:Number, min:1, max:3},
+    ID:String,
+    ValidatedID:{type:Number, min:1, max:3, default:1},
+    RGPDaccepted:Boolean,
+    Nacimiento:Date,
 });
 
 const Users = model('Users', userSchema);
