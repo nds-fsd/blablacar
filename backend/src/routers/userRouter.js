73a33f65f87be4cac210788f183b911c508fa5d3
Antonio Colella <me@antoniocolella.com>
2023-01-23 17:33:25 +0100

    sanitization of user registration

--- backend/src/routers/userRouter.js
+++ backend/src/routers/userRouter.js
@@ -3,12 +3,13 @@ import express from 'express';
 const userRouter = express.Router();
 import validateUserInput from "../Middleware/userMiddleware.js";
 import UsrControllers from '../controllers/user.js';
+import {body, check} from 'express-validator';
 //de momento suponemos que el schema de users de MongoDB
 //se llamará Users
 userRouter.get('/users',UsrControllers.usrGetAll);
-userRouter.post('/users', validateUserInput, UsrControllers.usrPost);
+userRouter.post('/users', body('email').normalizeEmail().trim(), check('email').isEmail(), validateUserInput, UsrControllers.usrPost);
 userRouter.get('/users/:id', UsrControllers.usrGetOne);
-userRouter.put('/users/:id', UsrControllers.usrPut);
+userRouter.put('/users/:id', body('email').normalizeEmail().trim(), check('email').isEmail(), UsrControllers.usrPut);
 userRouter.delete('/users/:id', UsrControllers.usrDelete);
 
 export {userRouter};
