f08883055768b3ea9ce7c734eacb540c9f86b03f
Aliikaass <alexandreacevedo92@gmail.com>
2023-02-23 23:26:23 +0100

    mensajes y chats funcionando 90%

--- backend/src/mongo/schemas/message.js
+++ backend/src/mongo/schemas/message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
 	sender: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}],
     content:  {type: String},
-    chat: [{type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true}],
+    chat: [{type: mongoose.Schema.Types.ObjectId, ref: 'Chat'}],
 },
 {timestamps: { createdAt: 'createdAt'}});
 const Message = mongoose.model('Message', messageSchema);
