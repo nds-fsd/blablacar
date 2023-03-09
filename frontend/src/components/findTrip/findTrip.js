1bebe0246343191351d4cfcf1547609fc7014a13
Fer Rosal <fernando.rosal@gmail.com>
2023-03-09 16:31:26 +0100

    Chat error fixed

--- frontend/src/components/findTrip/findTrip.js
+++ frontend/src/components/findTrip/findTrip.js
@@ -2,9 +2,11 @@ import styles from "./findTrip.module.css";
 import TripSummary from "../TripSummary/TripSummary";
 import { useEffect, useRef, useState} from "react";
 import { Request } from "../../utils/apiWrapper";
+import { Link } from "react-router-dom";
 
 const FindTrip = (props) => {
     const [trips,setTrips]=useState()
+    const [notFound,setNotFound]=useState(false)
   useEffect(() => {
     // Obtener los query params de la URL
     const searchParams = new URLSearchParams(window.location.search);
@@ -12,6 +14,9 @@ const FindTrip = (props) => {
     const getTrips=async()=>{
     // Realizar la petición HTTP con los query params en el cuerpo
     let getTrips = await Request("/trips","POST", queryParams)
+    getTrips.error===true?
+    setNotFound(true)
+    :
     setTrips(getTrips)
     }
     !trips&&getTrips()
@@ -27,7 +32,15 @@ const FindTrip = (props) => {
     
     return(
         <div className={styles.parappa}>
-            {trips&&trips.map(e=>
+            {notFound? 
+            <>
+            <h1 className={styles.notFound1}>Lo siento, no hay resultados para tu búsqueda</h1> 
+            <Link to="/">
+            <h3 className={styles.notFound2}>Volver</h3>
+            </Link>
+            </>
+            :
+            trips&&trips.map(e=>
 
             <div className={styles.wrapTrip}><TripSummary trip={e} id={e._id} setWhatModal={props.setWhatModal} setOpenModal={props.setOpenModal}></TripSummary></div>)
             }
